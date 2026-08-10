94e8cedf5f2dab41e7350ae729fe2333377f18c1
Miguel A Contreras <miguelacontreras@192.168.80.99>
2026-08-10 11:59:14 -0500

    chore: enforce eslint no-explicit-any as error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -32,7 +32,7 @@ export default tseslint.config(
       'unused-imports': unusedImports,
     },
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-explicit-any': 'error',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
       "prettier/prettier": ["error", { endOfLine: "auto" }],
